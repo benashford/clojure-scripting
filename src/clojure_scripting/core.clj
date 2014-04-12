84ca2e3d0505fed2ffee62ec931ffc357d385854
Ben Ashford <ben@bcash.org>
2014-04-12 20:34:08 +0100

    Initial commit

---
+++ src/clojure_scripting/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-scripting.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clojure_scripting/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-scripting.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-scripting.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
