e4514be3ddacbf0fb1d4b34ba1e372182e312f4d
Ben Ashford <ben@bcash.org>
2014-04-13 07:37:11 +0100

    Ability to evaluate a script in a supported language, and simple bindings

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojure-scripting "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.jruby/jruby-complete "1.7.11"]])
--- src/clojure_scripting/core.clj
+++ src/clojure_scripting/core.clj
@@ -1,6 +1,43 @@
-(ns clojure-scripting.core)
+(ns clojure-scripting.core
+  (:import [javax.script ScriptEngine ScriptEngineManager SimpleBindings]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-script-engine-manager []
+  (ScriptEngineManager.))
+
+(defn get-factory-details [factory]
+  {:engine-name (.getEngineName factory)
+   :engine-version (.getEngineVersion factory)
+   :factory factory
+   :language-name (.getLanguageName factory)
+   :language-version (.getLanguageVersion factory)})
+
+(defn get-engines
+  ([] (get-engines (make-script-engine-manager)))
+  ([sem]
+     (->> sem
+          .getEngineFactories
+          (map get-factory-details)
+          (map (fn [factory-details]
+                 [(:language-name factory-details) (dissoc factory-details :language-name)]))
+          (into {}))))
+
+(defn make-script-engine [language & [params]]
+  (let [params (or params {})
+        engine (.getEngineByName (make-script-engine-manager) language)]
+    (doseq [[k v] params]
+      (.put engine k v))
+    engine))
+
+(defn params->bindings [params]
+  (let [bindings (SimpleBindings.)]
+    (doseq [[k v] params]
+      (.put bindings k v))
+    bindings))
+
+(defmulti eval-script (fn [s & more] (class s)))
+(defmethod eval-script String [language script & [params]]
+  (-> language
+      make-script-engine
+      (eval-script script params)))
+(defmethod eval-script ScriptEngine [script-engine script & [params]]
+  (.eval script-engine script (params->bindings params)))
